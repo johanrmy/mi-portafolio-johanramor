1447dc1ef3a93510d719f1a62b6ab130181b8edb
Johan Ramos <johan.ramos.m@tecsup.edu.pe>
2024-07-21 14:20:00 -0500

    [add] vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { josefin_sans } from "@/app/fonts";
 import data from "@lib/data.json";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const { title, description, icon } = data.page.metadata;
 export const metadata: Metadata = {
@@ -17,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${josefin_sans.className}`}>{children}</body>
+      <body className={`${josefin_sans.className}`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
